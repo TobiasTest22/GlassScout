41ab97a3ae150254650718c8f9658fe6da8efc15
TobiasTest22 <TobiasTest22@users.noreply.github.com>
2026-07-02 20:04:11 +0300

    Publish installer build 0.1.14

--- src/domain/distribution.ts
+++ src/domain/distribution.ts
@@ -1,1 +1,1 @@
-export const windowsInstallerUrl = "https://github.com/TobiasTest22/GlassScout/releases/download/app-v0.1.6/GlassScout.FM26_0.1.6_x64-setup.exe";
+export const windowsInstallerUrl = "https://github.com/TobiasTest22/GlassScout/releases/download/app-v0.1.14/GlassScout.FM26_0.1.14_x64-setup.exe";
